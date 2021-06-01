e48b2a5dd79520b3d7bc5e316bfdb485dee8a0c0
allisonrenie <allisonrenie@gmail.com>
2021-05-31 18:34:57 -0700

    wrote test 4: check if mult of 3 and 5 returns FizzBuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,3 +7,5 @@ def test_print_num2():
     assert fizzbuzz.print_num(9) == 'Fizz'
 def test_print_num3():
     assert fizzbuzz.print_num(20) == 'Buzz'
+def test_print_num4():
+    assert fizzbuzz.print_num(15) == 'FizzBuzz'

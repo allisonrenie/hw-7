b32b6212f7fdc605c013b9e4193d36a912122c44
allisonrenie <allisonrenie@gmail.com>
2021-05-31 18:31:15 -0700

    wrote test 3: see if buzz returned for mult of 5

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,5 +3,7 @@ import pytest
 
 def test_print_num():
      assert fizzbuzz.print_num(14) == '14'
-def test_print_num():
+def test_print_num2():
     assert fizzbuzz.print_num(9) == 'Fizz'
+def test_print_num3():
+    assert fizzbuzz.print_num(20) == 'Buzz'

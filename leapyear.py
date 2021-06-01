4f570dd39abbb5655db0c57b469d8e1cd2caeb63
allisonrenie <allisonrenie@gmail.com>
2021-05-31 18:58:09 -0700

    wrote code for leapyr test 2: returns false if is mult of 4 and 100, or if is not mult of 4

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,10 @@
 def is_leapyear(year):
     byFour = int(year % 4)
     if byFour == 0:
-        return True
-    return False
+        by100 = int(year % 100)
+        if by100 == 0:
+            return False
+        else:
+            return True
+    else:
+        return False
